07f2b7f2c8b0a5a881fa5253fbd312d8aa75233c
清流 <ruoge3s@qq.com>
2020-09-15 11:54:18 +0800

    init

---
+++ src/TestServiceInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace unify\connector;
+
+/**
+ * 一个测试的接口
+ * Class TestServiceInterface
+ * @package unify\contract
+ */
+interface TestServiceInterface
+{
+    function out($a, $b);
+}
---
+++ src/UserServiceInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace unify\contract;
+
+interface UserServiceInterface
+{
+
+}
